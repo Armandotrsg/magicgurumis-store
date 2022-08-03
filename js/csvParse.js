bd39ba192827eff0787a41e3cf9629db7ca77d8d
Armando Terrazas <mandotrs@icloud.com>
2022-08-03 15:29:55 -0500

    Added the function to view a more detailed description of the product in the design sheet

--- js/csvParse.js
+++ js/csvParse.js
@@ -1,3 +1,18 @@
+function idGenerator(){
+  let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var alfa = abc[Math.floor(Math.random() * abc.length)+1];
+  var num = Math.floor(Math.random()*101);
+  return alfa+num.toString();
+}
+
+function goBackToProducts(){
+  let moreInfo = document.getElementById("moreInfo");
+  moreInfo.classList.add("d-none");
+
+  let productView = document.getElementById("productView");
+  productView.className = "";
+}
+
 function csvParse(file,rowN){
     $(document).ready(()=>{
         $.ajax({
@@ -5,11 +20,15 @@ function csvParse(file,rowN){
           dataType:"text",
           success:function(data){
             var productData = data.split(/\r?\n|\r/);
+            var allCards = [];
             for (var i = 0; i < productData.length; i++){
               var cellData = productData[i].split(',');
-              let card1 = new Product(cellData[0],cellData[1],cellData[2],cellData[3],cellData[4]);
-              card1.buildCard(rowN);
+              let card1 = new Product(cellData[0],cellData[1],cellData[2],cellData[3],cellData[4],cellData[5],cellData[6]);
+              card1.buildCard(rowN,cellData[5]);
+              allCards.push(card1);
+              
             }
+            return allCards;
           }
         })
 
--- js/product.js
+++ js/product.js
@@ -1,10 +1,12 @@
 class Product{
-    constructor(name,price,img,link,alt){ 
+    constructor(name,price,img,link,alt,id,description){ 
         this.name = name;
         this.price = price;
-        this.img = img;
+        this.img = "img/" + img;
         this.link = link;
         this.alt = alt; 
+        this.id = id;
+        this.description = description;
     }
 
     getName(){
@@ -22,8 +24,14 @@ class Product{
     getAlt(){
         return this.alt;
     }
+    getId(){
+        return this.id;
+    }
+    getDescription(){
+        return this.description;
+    }
 
-    buildCard(row_){
+    buildCard(row_,id_){
         let row = document.getElementById("row"+row_);
 
         let col = document.createElement("div");
@@ -31,16 +39,18 @@ class Product{
 
         let cardMain = document.createElement("div");
         cardMain.className = "card border-0 bg-light mb-2 mt-1";
+        //cardMain.setAttribute("id",id_);
 
         let cardBody = document.createElement("div");
         cardBody.className = "card-body"
 
         let productLink = document.createElement("a");
-        productLink.href = this.link;
-        productLink.target = "_blank";
+        productLink.href = "#";
+        productLink.setAttribute("id",id_);
+        productLink.onclick = function() {return buildMoreInfo(this)};
 
         let image = document.createElement("img");
-        image.src = "img/" + this.img;
+        image.src = this.img;
         image.alt = this.alt;
         image.className = "img-fluid";
 
@@ -59,5 +69,65 @@ class Product{
         col.appendChild(productTitle);
         col.appendChild(productPrice);
     }
+    
 
 }
+
+function changeView(product){
+    let productView = document.getElementById("productView");
+    productView.className = "d-none";
+
+    let section = document.getElementById("moreInfo");
+    section.classList.remove("d-none");
+  
+    let title1 = document.getElementById("productTitle1");
+    title1.innerHTML = product.getName();
+    
+    let title2 = document.getElementById("productTitle2");
+    title2.innerHTML = product.getName();
+
+    let image = document.getElementById("image-product");
+    image.src = product.getImg();
+    image.alt = product.getAlt();
+  
+    let description = document.getElementById("description");
+    description.innerHTML = product.getDescription();
+  
+    let price = document.getElementById("price");
+    price.innerHTML = "$" + product.getPrice();
+  
+    let facebook = document.getElementById("facebook");
+    facebook.href = product.getLink();
+    
+    return false;
+}
+
+function buildMoreInfo(element){
+    var file = "products/productList.csv";
+    var obj;
+    $(document).ready(()=>{
+        $.ajax({
+          url: file,
+          dataType:"text",
+          success:function(data){
+            var productData = data.split(/\r?\n|\r/);
+            var allCards = [];
+            
+            for (var i = 0; i < productData.length; i++){
+              var cellData = productData[i].split(',');
+              if(element.id == cellData[5]){
+                obj = new Product(cellData[0],cellData[1],cellData[2],cellData[3],cellData[4],cellData[5],cellData[6]);
+                changeView(obj);
+              }
+              
+            }
+            
+          }
+        });
+        
+  
+      });
+      return false;
+      
+}
+
